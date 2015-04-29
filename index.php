7bfb26b532a467c1b11f0651676f34fd23129961
MrRogue <dialgadiamond0@gmail.com>
2015-04-29 18:17:24 +0100

    Added core/main file

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php if (!empty($_POST)): ?>
+<div class="" style="" title="canddi-signature" id="canddi-signature"><table class="" style="width: 470px;" border="0" cellpadding="0" cellspacing="0" width="470">
+  <tbody class="" style="">
+    <tr class="" style="" valign="top">
+      <td class="" style="border-right: 1px solid rgb(69, 102, 142); padding-right: 10px; width: 10px;">
+        <a class="" style="" target="_blank" href="https://www.canddi.com/">
+          <img class="" src="https://s3.amazonaws.com/webapp.wisestamp.com/9r6oM48hQ2qiFloL42gB_canddi-logotype.png#logo" alt="photo" style="border-radius: 4px; width: 120px; max-width: 120px;">
+        </a>
+      </td>
+      <td class="" style="font: 14px Arial; color: rgb(100, 100, 100); padding-left: 10px;">
+        <div class="" style="">
+          <b style="" class=""><?php echo htmlspecialchars($_POST["name"]); ?></b>
+          <br class="" style="">
+          <span class="" style=""><?php echo htmlspecialchars($_POST["rank"]); ?></span>,
+          <span class="" style=""><?php echo htmlspecialchars($_POST["company"]); ?></span>
+        </div>
+        <div class="" style="color: rgb(141, 141, 141); font-size: 13px; padding: 5px 0px;">
+          <span class="" style="color: rgb(69, 102, 142); display: inline-block;">p:</span>
+          <a class="" target="_blank" href="tel:<?php echo htmlspecialchars($_POST["landline"]); ?>" style="color: rgb(141, 141, 141); text-decoration: none;"><?php echo htmlspecialchars($_POST["landline"]); ?></a>  |
+          <span class="" style="display: inline-block;">
+            <span class="" style="color: rgb(69, 102, 142);">m:</span>
+            <a class="" target="_blank" href="tel:<?php echo htmlspecialchars($_POST["mobile"]); ?>" style="color: rgb(141, 141, 141); text-decoration: none;"><?php echo htmlspecialchars($_POST["mobile"]); ?></a>
+          </span>  |
+          <span class="" style="display: inline-block;">
+            <span class="" style="color: rgb(69, 102, 142);">e:</span>
+            <a class="" href="mailto:<?php echo htmlspecialchars($_POST["email"]); ?>" style="color: rgb(141, 141, 141); text-decoration: none;"><?php echo htmlspecialchars($_POST["email"]); ?></a>
+          </span>  |
+          <span class="" style="white-space: nowrap; display: inline-block;">
+            <span class="" style="color: rgb(69, 102, 142);">w:</span>
+            <a class="" href="https://www.canddi.com/" target="_blank" style="color: rgb(141, 141, 141); text-decoration: none;">
+              www.canddi.com
+            </a>
+          </span>
+        </div>
+        <div class="" style="margin-top: 5px;">
+          <a class="" href="http://uk.linkedin.com/in/langleytim?ce_b4=YS5tYWxpa0BhYnlzc3RlY2guY28udWs%3D&amp;cas_b4=dGltQHRpbWxhbmdsZXkubWUudWs%3D&amp;utm_medium=email&amp;utm_source=gmail" target="_blank" style="text-decoration: none;">
+            <img class="" style="" src="https://s3.amazonaws.com/images.wisestamp.com/icons/linkedin.png" height="16" width="16">
+          </a>&nbsp;
+          <a class="" href="http://twitter.com/canddi?ce_b4=YS5tYWxpa0BhYnlzc3RlY2guY28udWs%3D&amp;cas_b4=dGltQHRpbWxhbmdsZXkubWUudWs%3D&amp;utm_medium=email&amp;utm_source=gmail" target="_blank" style="text-decoration: none;">
+            <img class="" style="" src="https://s3.amazonaws.com/images.wisestamp.com/icons/twitter.png" height="16" width="16">
+          </a>&nbsp;
+          <a class="" href="http://www.youtube.com/channel/UCU7aljz8YC9IdPfxuxLY39g?ce_b4=YS5tYWxpa0BhYnlzc3RlY2guY28udWs%3D&amp;cas_b4=dGltQHRpbWxhbmdsZXkubWUudWs%3D&amp;utm_medium=email&amp;utm_source=gmail" target="_blank" style="text-decoration: none;">
+            <img class="" style="" src="https://s3.amazonaws.com/images.wisestamp.com/icons/youtube.png" height="16" width="16">
+          </a>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table></div>
+<?php echo htmlspecialchars('
+<div class="" style="" title="canddi-signature" id="canddi-signature"><table class="" style="width: 470px;" border="0" cellpadding="0" cellspacing="0" width="470">
+  <tbody class="" style="">
+    <tr class="" style="" valign="top">
+      <td class="" style="border-right: 1px solid rgb(69, 102, 142); padding-right: 10px; width: 10px;">
+        <a class="" style="" target="_blank" href="https://www.canddi.com/">
+          <img class="" src="https://s3.amazonaws.com/webapp.wisestamp.com/9r6oM48hQ2qiFloL42gB_canddi-logotype.png#logo" alt="photo" style="border-radius: 4px; width: 120px; max-width: 120px;">
+        </a>
+      </td>
+      <td class="" style="font: 14px Arial; color: rgb(100, 100, 100); padding-left: 10px;">
+        <div class="" style="">
+          <b style="" class=""><?php echo htmlspecialchars($_POST["name"]); ?></b>
+          <br class="" style="">
+          <span class="" style=""><?php echo htmlspecialchars($_POST["rank"]); ?></span>,
+          <span class="" style=""><?php echo htmlspecialchars($_POST["company"]); ?></span>
+        </div>
+        <div class="" style="color: rgb(141, 141, 141); font-size: 13px; padding: 5px 0px;">
+          <span class="" style="color: rgb(69, 102, 142); display: inline-block;">p:</span>
+          <a class="" target="_blank" href="tel:<?php echo htmlspecialchars($_POST["landline"]); ?>" style="color: rgb(141, 141, 141); text-decoration: none;"><?php echo htmlspecialchars($_POST["landline"]); ?></a>  |
+          <span class="" style="display: inline-block;">
+            <span class="" style="color: rgb(69, 102, 142);">m:</span>
+            <a class="" target="_blank" href="tel:<?php echo htmlspecialchars($_POST["mobile"]); ?>" style="color: rgb(141, 141, 141); text-decoration: none;"><?php echo htmlspecialchars($_POST["mobile"]); ?></a>
+          </span>  |
+          <span class="" style="display: inline-block;">
+            <span class="" style="color: rgb(69, 102, 142);">e:</span>
+            <a class="" href="mailto:<?php echo htmlspecialchars($_POST["email"]); ?>" style="color: rgb(141, 141, 141); text-decoration: none;"><?php echo htmlspecialchars($_POST["email"]); ?></a>
+          </span>  |
+          <span class="" style="white-space: nowrap; display: inline-block;">
+            <span class="" style="color: rgb(69, 102, 142);">w:</span>
+            <a class="" href="https://www.canddi.com/" target="_blank" style="color: rgb(141, 141, 141); text-decoration: none;">
+              www.canddi.com
+            </a>
+          </span>
+        </div>
+        <div class="" style="margin-top: 5px;">
+          <a class="" href="http://uk.linkedin.com/in/langleytim?ce_b4=YS5tYWxpa0BhYnlzc3RlY2guY28udWs%3D&amp;cas_b4=dGltQHRpbWxhbmdsZXkubWUudWs%3D&amp;utm_medium=email&amp;utm_source=gmail" target="_blank" style="text-decoration: none;">
+            <img class="" style="" src="https://s3.amazonaws.com/images.wisestamp.com/icons/linkedin.png" height="16" width="16">
+          </a>&nbsp;
+          <a class="" href="http://twitter.com/canddi?ce_b4=YS5tYWxpa0BhYnlzc3RlY2guY28udWs%3D&amp;cas_b4=dGltQHRpbWxhbmdsZXkubWUudWs%3D&amp;utm_medium=email&amp;utm_source=gmail" target="_blank" style="text-decoration: none;">
+            <img class="" style="" src="https://s3.amazonaws.com/images.wisestamp.com/icons/twitter.png" height="16" width="16">
+          </a>&nbsp;
+          <a class="" href="http://www.youtube.com/channel/UCU7aljz8YC9IdPfxuxLY39g?ce_b4=YS5tYWxpa0BhYnlzc3RlY2guY28udWs%3D&amp;cas_b4=dGltQHRpbWxhbmdsZXkubWUudWs%3D&amp;utm_medium=email&amp;utm_source=gmail" target="_blank" style="text-decoration: none;">
+            <img class="" style="" src="https://s3.amazonaws.com/images.wisestamp.com/icons/youtube.png" height="16" width="16">
+          </a>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table></div>
+'); ?>
+<?php else: ?>
+    <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+        Name: <input type="text" name="name"><br>
+		Rank: <input type="text" name="rank"><br>
+		Company: <input type="text" name="company"><br>
+		Landline: <input type="text" name="landline"><br>
+		Mobile: <input type="text" name="mobile"><br>
+        Email: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+<?php endif; ?>
